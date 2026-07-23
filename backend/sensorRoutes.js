eb63e9ab8f4a6489640f9b526bd22ffd248be721
Sanjay Prathish V <vnsdnandhini@gmail.com>
2026-07-23 14:04:26 +0530

    Fix missing path import

--- backend/sensorRoutes.js
+++ backend/sensorRoutes.js
@@ -5,11 +5,11 @@
  * by Postgres instead of file storage.
  */
 const fs = require('fs');
+const path = require('path');
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
-}
-const express = require('express');
+}const express = require('express');
 const { findBestMatch } = require('./spectrumSimilarity');
 
 module.exports = function (pool, generateId) {
